cdd089c7f0b0e1098f514269bfc9cab79f0b5a27
Léo Maradan <3043485+Leomaradan@users.noreply.github.com>
2025-03-13 18:04:27 +0100

    Bundle config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig, UserConfigExport } from 'vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const baseConfig: UserConfigExport = {
+    base: '/hypixel-attribute-search/',
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            mui: ['@mui/material', '@mui/icons-material']
+          }
+        }
+      }
+    },
+    plugins: [react()]
+  };
+  if (command === 'serve') {
+    return {
+      ...baseConfig,
+      define: {
+        global: {}
+      }
+    };
+  } else {
+    // command === 'build'
+    return {
+      ...baseConfig
+    };
+  }
+});
